e2421cfb67cc4e2c1548f7e4455ae6f8e577b575
Daniele Lisi <danielelisi978@gmail.com>
2016-12-15 00:56:22 -0800

    Reupload files

--- js.js
+++ js.js
@@ -3,12 +3,19 @@ var buyBut = document.getElementById("buy-btn");
 
 var lightboxDiv = document.getElementById("lightbox");
 var closeBut = document.getElementById("close-but");
-
+var lightContent = document.getElementById("cart-div");
 
 closeBut.onclick = function () {
     lightboxDiv.style.display = "none";
 }
 
 cartBut.onclick = function() {
-    lightboxDiv.style.display = "fixed";
+    lightboxDiv.style.display = "block";
+}
+
+var idx = 0
+buyBut.onclick = function () {
+	lightboxDiv.style.display = "block";
+	idx = idx + 1;
+	lightContent.innerHTML ="FasTap x " + idx;
 }
