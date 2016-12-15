7bd1c6330454a2d9d4302520068a0aeea0609742
Daniele Lisi <danielelisi978@gmail.com>
2016-12-15 01:55:27 -0800

    final version

--- js.js
+++ js.js
@@ -4,6 +4,12 @@ var buyBut = document.getElementById("buy-btn");
 var lightboxDiv = document.getElementById("lightbox");
 var closeBut = document.getElementById("close-but");
 var lightContent = document.getElementById("cart-div");
+var amountDiv = document.getElementById("amount");
+var lightContainer = document.getElementById("lightbox-container");
+var amountpricediv = document.getElementById("totalpricediv");
+
+var totalPrice = document.createElement("div");
+amountpricediv.appendChild(totalPrice);
 
 closeBut.onclick = function () {
     lightboxDiv.style.display = "none";
@@ -13,9 +19,15 @@ cartBut.onclick = function() {
     lightboxDiv.style.display = "block";
 }
 
-var idx = 0
+var price = 200;
+var idx = 0;
 buyBut.onclick = function () {
 	lightboxDiv.style.display = "block";
 	idx = idx + 1;
-	lightContent.innerHTML ="FasTap x " + idx;
+    price = price * idx;
+    amountDiv.innerHTML = "FasTap x "+idx;
+    totalPrice.className = "label label-primary totalamount center-block";
+    totalPrice.innerHTML = "Total Amount: $"+ price;
+    
+    
 }
